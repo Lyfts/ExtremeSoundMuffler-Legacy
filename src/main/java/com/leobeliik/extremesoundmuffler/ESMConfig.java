7f2f17a9aeb6312eb1b565976a6031e526be8c7f
Lyft <127234178+Lyfts@users.noreply.github.com>
2026-06-04 16:15:02 +0200

    Allow muffling thunder & rain by default (#34)

--- src/main/java/com/leobeliik/extremesoundmuffler/ESMConfig.java
+++ src/main/java/com/leobeliik/extremesoundmuffler/ESMConfig.java
@@ -16,7 +16,7 @@ public class ESMConfig {
 
     public static class General {
 
-        @Config.DefaultStringList({ "ui.", "music.", "ambient." })
+        @Config.DefaultStringList({ "ui.", "music." })
         @Config.Comment("Blacklisted Sounds - add the name of the sounds to blacklist, separated with comma")
         public String[] forbiddenSounds;
 
